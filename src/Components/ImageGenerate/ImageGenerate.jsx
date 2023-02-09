d9b2258786ef391ae2b0178c2845be62bd01019a
megha <meghamini40@gmail.com>
2023-02-09 14:50:18 +0530

    image generater

---
+++ src/Components/ImageGenerate/ImageGenerate.jsx
@@ -0,0 +1,63 @@
+import { Configuration, OpenAIApi } from "openai";
+import { useState } from "react";
+
+const ImageGenerate = () => {
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_APIKEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState([]);
+
+  const handleClick = async () => {
+    // e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await openai.createImage({
+        prompt: search,
+        n: 1,
+        size: "1024x1024",
+      });
+      setImageUrl((prevState) => [
+        ...prevState,
+        {
+          id: imageUrl.length + 1,
+          image: search,
+          result: response.data.data[0].url,
+        },
+      ]);
+    } catch (err) {
+      console.log(err);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <main>
+      {imageUrl &&
+        imageUrl.map(({ id, image, result }) => {
+          return (
+            <div key={id}>
+              <p>{image}</p>
+              <img src={result} alt="" width="200px" />
+            </div>
+          );
+        })}
+      <div>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="what type of image you want..."
+        />
+        <button onClick={handleClick}>
+          {loading ? "generating..." : "image generate"}
+        </button>
+      </div>
+    </main>
+  );
+};
+
+export default ImageGenerate;
