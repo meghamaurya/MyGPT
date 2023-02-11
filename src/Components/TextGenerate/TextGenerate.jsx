020ea5fbdfbb27252e7bf4bf4d288cd45a7bb2eb
megha <meghamini40@gmail.com>
2023-02-11 14:52:29 +0530

    modified typewirter

--- src/Components/TextGenerate/TextGenerate.jsx
+++ src/Components/TextGenerate/TextGenerate.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { Configuration, OpenAIApi } from "openai";
+import Typewriter from "typewriter-effect";
+import "./TextGenerate.scss";
+import Nav from "../Nav/Nav";
 
 const TextGenerate = () => {
   const configuration = new Configuration({
@@ -13,6 +16,7 @@ const TextGenerate = () => {
     { id: "", text: "", result: "" },
   ]);
   //   const [error, setError] = useState("");
+  const [stopType, setStopType] = useState(false);
 
   const handleClick = async () => {
     setLoading(true);
@@ -69,19 +73,47 @@ const TextGenerate = () => {
       setLoading(false);
     }
   };
+
   return (
-    <main className="main">
-      <div className="container">
+    <main className="textGenerator">
+      <Nav />
+      <div className="textContainer">
         {messageResult.map(({ id, message, result }) => (
-          <>
-            <div key={id}>{message}</div>
-            <pre className="result">{result}</pre>
-          </>
+          <div key={id}>
+            {result ? (
+              <>
+                <div className="searchMsg">{message}</div>
+                <Typewriter
+                  className="typewriter"
+                  onInit={(typewriter) => {
+                    stopType
+                      ? typewriter.typeString(result).stop()
+                      : typewriter.typeString(result).start();
+                  }}
+                />
+                <button
+                  onClick={() => {
+                    setStopType(true);
+                    console.log("stop");
+                  }}
+                >
+                  stop
+                </button>
+              </>
+            ) : (
+              <div className="searchMsg">{message}</div>
+            )}
+
+            {/* <pre className="result">{result}</pre> */}
+          </div>
         ))}
         <input
           typeof="text"
           value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
+          onChange={(e) => {
+            setPrompt(e.target.value);
+            setStopType(false);
+          }}
           onKeyDown={handleKeyEvent}
           placeholder="type here..."
           className="input"
