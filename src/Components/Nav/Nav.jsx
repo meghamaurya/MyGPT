aa199215cd592ee4e661bf113c6ae77e9190066a
megha <meghamini40@gmail.com>
2023-02-11 14:54:18 +0530

    refactor code

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import "./Nav.scss";
+import logo from "../Images/chat.gif";
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <div className="logoItems">
+        <img src={logo} alt="logo" className="logo" />
+        <span className="appName">MyGPT</span>
+      </div>
+      <div className="linkContent">
+        <Link to="/" className="link">
+          Text Generate
+        </Link>
+        <Link to="imageGenerate" className="link">
+          Image Generate
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
